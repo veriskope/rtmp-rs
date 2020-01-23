16f0ca6ca42de5599347ba4e20ea900851435a24
Sarah Allen <sarah@ultrasaurus.com>
2020-01-23 13:02:56 -0800

    add handshake code from rust-media-libs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use tokio::prelude::*;
 use tokio::{io::BufReader, net::TcpStream};
 pub mod error;
-
+mod handshake;
+use handshake::{HandshakeProcessResult, Handshake, PeerType};
 
 pub struct Connection<T = TcpStream> {
     url: String,
-    bufconn: BufReader<T>
+    cn: BufReader<T>
 }
 
 impl<Transport: AsyncRead + AsyncWrite + Unpin> Connection<Transport> {
@@ -13,22 +14,21 @@ impl<Transport: AsyncRead + AsyncWrite + Unpin> Connection<Transport> {
     pub fn new(url: String, transport: Transport) -> Self {
       Connection {
         url,
-        bufconn: BufReader::new(transport),
+        cn: BufReader::new(transport),
       }
     }
     //pub async fn connect(&mut self) -> Result<(), Error> {
     // error[E0412]: cannot find type `Error` in this scope
     pub async fn connect(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        println!("TODO -- implement connect");
+
+        let mut handshake = Handshake::new(PeerType::Client);
+        let c0_and_c1 = handshake.generate_outbound_p0_and_p1().unwrap();
+        self.cn.write(&c0_and_c1).await.expect("write c0_and_c1");
+    
+    
+
         Ok(())
     }
   
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
