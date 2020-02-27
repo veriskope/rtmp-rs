008e4119f41207fda3734be77728d55a5e2f7274
Sarah Allen <sarah@ultrasaurus.com>
2020-02-27 13:57:45 -0800

    runtime -> Arc<Mutex<Runtime>>

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,12 +1,14 @@
 use log::{info, trace, warn};
+use std::sync::{Arc, Mutex};
+
 use std::sync::atomic::{AtomicUsize, Ordering};
+use tokio::runtime::Runtime;
 use tokio::sync::mpsc;
 use url::Url;
 
 use crate::amf::Value;
 use crate::stream::*;
 use crate::Message;
-use tokio::runtime::Runtime;
 
 mod inner;
 use inner::InnerConnection;
@@ -20,98 +22,105 @@ pub mod handshake;
 const CHANNEL_SIZE: usize = 100;
 
 pub struct Connection {
-    url: Url,
-    runtime: Runtime,
-    next_cmd_id: AtomicUsize,
-    to_server_tx: mpsc::Sender<Message>, // messages destined for server go here
-    to_server_rx: Option<mpsc::Receiver<Message>>, // process loop grabs them from here
+  url: Url,
+  runtime_guard: Arc<Mutex<Runtime>>,
+  next_cmd_id: AtomicUsize,
+  to_server_tx: mpsc::Sender<Message>, // messages destined for server go here
+  to_server_rx: Option<mpsc::Receiver<Message>>, // process loop grabs them from here
 }
 
 impl Connection {
-    // todo: new_with_transport, then new can defer creation of connection
-    pub fn new(url: Url) -> Self {
-        info!(target: "rtmp::Connection", "new");
-        let runtime = tokio::runtime::Runtime::new().unwrap();
-        let (to_server_tx, to_server_rx) = mpsc::channel::<Message>(CHANNEL_SIZE);
+  // todo: new_with_transport, then new can defer creation of connection
+  pub fn new(url: Url) -> Self {
+    info!(target: "rtmp::Connection", "new");
+    let runtime = tokio::runtime::Runtime::new().expect("new Runtime");
+    let (to_server_tx, to_server_rx) = mpsc::channel::<Message>(CHANNEL_SIZE);
 
-        Connection {
-            url,
-            runtime,
-            next_cmd_id: AtomicUsize::new(2),
-            to_server_tx,
-            to_server_rx: Some(to_server_rx), // consumed by process_loop
-        }
+    Connection {
+      url,
+      runtime_guard: Arc::new(Mutex::new(runtime)),
+      next_cmd_id: AtomicUsize::new(2),
+      to_server_tx,
+      to_server_rx: Some(to_server_rx), // consumed by process_loop
     }
+  }
 
-    fn get_next_cmd_id(&self) -> f64 {
-        self.next_cmd_id.fetch_add(1, Ordering::SeqCst) as f64
-    }
+  fn get_next_cmd_id(&self) -> f64 {
+    self.next_cmd_id.fetch_add(1, Ordering::SeqCst) as f64
+  }
 
-    pub fn new_stream(&mut self) -> NetStream {
-        // looks like stream id is created on the server
-        let cmd_id = self.get_next_cmd_id();
-        let to_server_tx = self.to_server_tx.clone();
-        self.runtime.block_on( async move {
-          create_stream(cmd_id, to_server_tx ).await;
-        });
-        NetStream::Command(cmd_id)
-    }
+  pub fn new_stream(&mut self) -> NetStream {
+    // looks like stream id is created on the server
+    let cmd_id = self.get_next_cmd_id();
+    let to_server_tx = self.to_server_tx.clone();
+    let mut runtime = self.runtime_guard.lock().unwrap();
 
-    //     to_server_rx: ownership moves to the spawned thread, its job is to
-    //                  recv messages on this channel and send 'em to the server
-    //  from_server_tx: the thread also listens on the socket, reads messages
-    //                  and sends them on this channel
-    fn spawn_socket_process_loop(
-        &mut self,
-        to_server_rx: mpsc::Receiver<Message>,
-        from_server_tx: mpsc::Sender<Message>,
-    ) {
-        let url = self.url.clone();
-        let _cn_handle = self.runtime.spawn(async move {
-            trace!(target: "rtmp:connect_with_callback", "spawn socket handler");
-            let mut cn = InnerConnection::new(url, to_server_rx).await;
-            cn.connect().await.expect("rtmp connection");
-            cn.process_message_loop(from_server_tx)
-                .await
-                .expect("read until socket closes");
-        });
-    }
+    runtime.block_on(async move {
+      create_stream(cmd_id, to_server_tx).await;
+    });
+    NetStream::Command(cmd_id)
+  }
 
-    pub fn spawn_message_receiver(
-        &mut self,
-        f: impl Fn(Message) -> () + Send + 'static,
-        mut from_server_rx: mpsc::Receiver<Message>,
-    ) {
-        let to_server_tx = self.to_server_tx.clone();
-        let _res_handle = self.runtime.spawn(async move {
-          trace!(target: "rtmp:message_receiver", "spawn recv handler");
-          let mut num:i32 = 1; // just for debugging
-          loop {
-            let msg = from_server_rx.recv().await.expect("recv from server");
-            trace!(target: "rtmp:message_receiver", "#{}) recv from server {:?}", num, msg);
-            if let Some(status) = msg.get_status() {
-              let v: Vec<&str> = status.code.split('.').collect();
-              match v[0] {
-                "NetConnection" => f(msg),
-                _ => warn!(target: "rtmp:message_receiver", "unhandled status {:?}", status),
-              }
-            } else {
+  //     to_server_rx: ownership moves to the spawned thread, its job is to
+  //                  recv messages on this channel and send 'em to the server
+  //  from_server_tx: the thread also listens on the socket, reads messages
+  //                  and sends them on this channel
+  fn spawn_socket_process_loop(
+    &mut self,
+    to_server_rx: mpsc::Receiver<Message>,
+    from_server_tx: mpsc::Sender<Message>,
+  ) {
+    let url = self.url.clone();
+    let mut runtime = self.runtime_guard.lock().unwrap();
+    let _cn_handle = runtime.spawn(async move {
+      trace!(target: "rtmp:connect_with_callback", "spawn socket handler");
+      let mut cn = InnerConnection::new(url, to_server_rx).await;
+      cn.connect().await.expect("rtmp connection");
+      cn.process_message_loop(from_server_tx)
+        .await
+        .expect("read until socket closes");
+    });
+  }
+
+  pub fn spawn_message_receiver(
+    &mut self,
+    f: impl Fn(Message) -> () + Send + 'static,
+    mut from_server_rx: mpsc::Receiver<Message>,
+  ) {
+    let to_server_tx = self.to_server_tx.clone();
+    let mut runtime = self.runtime_guard.lock().unwrap();
+
+    let _res_handle = runtime.spawn(async move {
+      trace!(target: "rtmp:message_receiver", "spawn recv handler");
+      let mut num: i32 = 1; // just for debugging
+      loop {
+        let msg = from_server_rx.recv().await.expect("recv from server");
+        trace!(target: "rtmp:message_receiver", "#{}) recv from server {:?}", num, msg);
+        if let Some(status) = msg.get_status() {
+          let v: Vec<&str> = status.code.split('.').collect();
+          match v[0] {
+            "NetConnection" => f(msg),
+            _ => warn!(target: "rtmp:message_receiver", "unhandled status {:?}", status),
+          }
+        } else {
           match msg {
-            Message::Response { id, data:_, opt } => {
+            Message::Response { id, data: _, opt } => {
               if id == 2.0 {
-                match opt { 
+                match opt {
                   Value::Number(stream_id) => {
                     // create stream
                     trace!(target: "rtmp:message_receiver", "publish");
-                    publish(stream_id as u32,
+                    publish(
+                      stream_id as u32,
                       to_server_tx.clone(),
                       "cameraFeed".to_string(),
                       RecordFlag::Live,
-                    ).await;
-                  },
+                    )
+                    .await;
+                  }
                   _ => {
                     warn!(target: "rtmp:message_receiver", "unexpected opt type {:?}", opt);
-                  } 
+                  }
                 } // match opt
               } // id == 2.0
             }
@@ -123,22 +132,22 @@ impl Connection {
         num += 1;
       }
     });
-    }
+  }
 
-    // std API that returns immediately, then calls callback later
-    pub fn connect_with_callback(
-        &mut self,
-        f: impl Fn(Message) -> () + Send + 'static,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        trace!(target: "rtmp:connect_with_callback", "url: {}", self.url);
-        let (from_server_tx, from_server_rx) = mpsc::channel::<Message>(CHANNEL_SIZE);
-        // ownership will move to thread for processing messages
-        let to_server_rx_option = std::mem::replace(&mut self.to_server_rx, None);
-        let to_server_rx = to_server_rx_option.expect("to_server_rx should be defined");
+  // std API that returns immediately, then calls callback later
+  pub fn connect_with_callback(
+    &mut self,
+    f: impl Fn(Message) -> () + Send + 'static,
+  ) -> Result<(), Box<dyn std::error::Error>> {
+    trace!(target: "rtmp:connect_with_callback", "url: {}", self.url);
+    let (from_server_tx, from_server_rx) = mpsc::channel::<Message>(CHANNEL_SIZE);
+    // ownership will move to thread for processing messages
+    let to_server_rx_option = std::mem::replace(&mut self.to_server_rx, None);
+    let to_server_rx = to_server_rx_option.expect("to_server_rx should be defined");
 
-        self.spawn_socket_process_loop(to_server_rx, from_server_tx);
-        self.spawn_message_receiver(f, from_server_rx);
+    self.spawn_socket_process_loop(to_server_rx, from_server_tx);
+    self.spawn_message_receiver(f, from_server_rx);
 
-        Ok(())
-    }
+    Ok(())
+  }
 }
