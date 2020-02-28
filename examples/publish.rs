7be1c41474ea7e53ac2c215348eb4aecb2ceb115
Sarah Allen <sarah@ultrasaurus.com>
2020-02-27 20:31:05 -0800

    refactor creation of mpsc

--- examples/publish.rs
+++ examples/publish.rs
@@ -4,35 +4,35 @@ use std::time::Duration;
 use url::Url;
 
 fn stream_callback(msg: rtmp::Message) {
-    println!("===================> stream_callback: {:?}", msg);
+  println!("===================> stream_callback: {:?}", msg);
 }
 
 fn main() {
-    pretty_env_logger::init();
+  pretty_env_logger::init();
 
-    let addr = "127.0.0.1:1935";
+  let addr = "127.0.0.1:1935";
 
-    let url = Url::parse(&format!("rtmp://{}/live/mystream", addr)).expect("url parse");
+  let url = Url::parse(&format!("rtmp://{}/live/mystream", addr)).expect("url parse");
 
-    let mut conn = rtmp::Connection::new(url, Some(stream_callback));
-    //let stream =
-    conn.new_stream();
+  let mut conn = rtmp::Connection::new(url, Some(stream_callback));
+  //let stream = conn.new_stream();
 
-    // optional set timeout to 1 sec: conn.set_timeout(1000);
-    conn.connect_with_callback(move |response| {
-        println!("===> connect response: {:?}", response);
-        // TODO hack: publish called automatically on successful create
-        // stream.publish("mystream".to_string(), rtmp::RecordFlag::Live);
-        // println!("===> published: {}", stream);
+  // optional set timeout to 1 sec: conn.set_timeout(1000);
+  conn
+    .connect_with_callback(move |response| {
+      println!("===> connect response: {:?}", response);
+      // TODO hack: publish called automatically on successful create
+      // stream.publish("mystream".to_string(), rtmp::RecordFlag::Live);
+      // println!("===> published: {}", stream);
     })
     .expect("rtmp connect");
-    println!("waiting");
+  println!("waiting");
 
-    let some_time = Duration::from_millis(100);
-    loop {
-        sleep(some_time);
-    }
-    // let mut input = String::new();
-    // println!("press return to quit");
-    // std::io::stdin().read_line(&mut input).expect("stdio read_line");
+  let some_time = Duration::from_millis(100);
+  loop {
+    sleep(some_time);
+  }
+  // let mut input = String::new();
+  // println!("press return to quit");
+  // std::io::stdin().read_line(&mut input).expect("stdio read_line");
 }
