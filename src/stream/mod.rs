b1c8604377820ee17ab2461b696351aa5615da51
Sarah Allen <sarah@ultrasaurus.com>
2020-03-02 16:02:34 -0800

    fix all warnings (except for test-only code)

--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -2,14 +2,14 @@ mod flag;
 use crate::amf::Value;
 use crate::Message;
 pub use flag::RecordFlag;
-use log::trace;
+// use log::trace;
 use tokio::sync::mpsc;
 
 pub enum NetStream {
-    // idea for a state machine, not sure this is the right approach
-    Command(f64),
-    Created(f64),
-    // PublishRequest(String, RecordFlag),
+  // idea for a state machine, not sure this is the right approach
+  Command(f64),
+  Created(f64),
+  // PublishRequest(String, RecordFlag),
 }
 
 // pub struct NetStreamInfo {
@@ -18,37 +18,37 @@ pub enum NetStream {
 // }
 
 pub async fn create_stream(cmd_id: f64, mut tx_to_server: mpsc::Sender<Message>) {
-    let msg = Message::Command {
-        name: "createStream".to_string(),
-        id: cmd_id,
-        data: Value::Null,
-        opt: Vec::new(),
-    };
-    tx_to_server
-        .send(msg)
-        .await
-        .expect("queue 'createStream' message to server");
+  let msg = Message::Command {
+    name: "createStream".to_string(),
+    id: cmd_id,
+    data: Value::Null,
+    opt: Vec::new(),
+  };
+  tx_to_server
+    .send(msg)
+    .await
+    .expect("queue 'createStream' message to server");
 }
 
-pub async fn publish(
-    stream_id: u32,
-    mut tx_to_server: mpsc::Sender<Message>,
-    name: String,
-    flag: RecordFlag,
-) {
-    let params = vec![Value::Utf8(name.clone()), Value::Utf8(flag.to_string())];
+// pub async fn publish(
+//   stream_id: u32,
+//   mut tx_to_server: mpsc::Sender<Message>,
+//   name: String,
+//   flag: RecordFlag,
+// ) {
+//   let params = vec![Value::Utf8(name.clone()), Value::Utf8(flag.to_string())];
 
-    let msg = Message::StreamCommand {
-        name: "publish".to_string(),
-        stream_id,
-        params,
-    };
-    trace!(target: "publish", "tx_to_server: {:?}", msg);
-    tx_to_server
-        .send(msg)
-        .await
-        .expect("queue 'publish' message to server");
-}
+//   let msg = Message::StreamCommand {
+//     name: "publish".to_string(),
+//     stream_id,
+//     params,
+//   };
+//   trace!(target: "publish", "tx_to_server: {:?}", msg);
+//   tx_to_server
+//     .send(msg)
+//     .await
+//     .expect("queue 'publish' message to server");
+// }
 
 // impl fmt::Display for NetStream {
 //   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
